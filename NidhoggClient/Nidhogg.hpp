19ec072e068cea570f2a12371fccfb0c381e9e90
idov31 <62358580+Idov31@users.noreply.github.com>
2022-06-01 20:32:08 +0300

    Fixed protected processes list error

--- NidhoggClient/Nidhogg.hpp
+++ NidhoggClient/Nidhogg.hpp
@@ -54,18 +54,20 @@ int NidhoggProcessUnprotect(std::vector<DWORD> pids) {
     return NIDHOGG_SUCCESS;
 }
 
-DWORD* NidhoggListProtectedProcesses(std::vector<DWORD> pids) {
-    DWORD pidsList[MAX_PIDS] = {0};
+int NidhoggListProtectedProcesses(DWORD pidsList[MAX_PIDS]) {
     DWORD bytes;
     HANDLE hFile = CreateFile(L"\\\\.\\Nidhogg", GENERIC_WRITE | GENERIC_READ, 0, nullptr, OPEN_EXISTING, 0, nullptr);
 
 	if (hFile == INVALID_HANDLE_VALUE)
-		return pidsList;
+		return NIDHOGG_ERROR_CONNECT_DRIVER;
 
-    DeviceIoControl(hFile, IOCTL_NIDHOGG_CLEAR_PID_PROTECTION, &pidsList, sizeof(pidsList), nullptr, 0, &bytes, nullptr);    
+    if (!DeviceIoControl(hFile, IOCTL_NIDHOGG_CLEAR_PID_PROTECTION, &pidsList, sizeof(pidsList), nullptr, 0, &bytes, nullptr)) {
+        CloseHandle(hFile);
+        return NIDHOGG_ERROR_DEVICECONTROL_DRIVER;
+    }
 
     CloseHandle(hFile);
-    return pidsList;
+    return NIDHOGG_SUCCESS;
 }
 
 int NidhoggProcessClearAllProtection() {
