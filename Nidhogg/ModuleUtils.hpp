3d4ef94cc331dabc18674a6b690ae79b049f8bd6
Ido Veltzman <62358580+Idov31@users.noreply.github.com>
2022-10-16 17:28:01 +0300

    Function patching works

--- Nidhogg/ModuleUtils.hpp
+++ Nidhogg/ModuleUtils.hpp
@@ -14,19 +14,21 @@
 NTSTATUS PatchModule(PatchedModule* ModuleToPatch) {
 	HANDLE hTargetProcess;
 	PEPROCESS TargetProcess;
-	PEPROCESS CurrentProcess;
 	ULONG oldProtection;
 	KAPC_STATE state;
-	
+	SIZE_T written;
+
+	PVOID functionAddress = NULL;
 	PVOID moduleImageBase = NULL;
 	NTSTATUS status = STATUS_UNSUCCESSFUL;
-	bool success = true;
 	LARGE_INTEGER time = { 0 };
 	time.QuadPart = -100ll * 10 * 1000;
 
 	// Validate that the required functions are loaded correctly.
-	if (!dimGlobals.ZwProtectVirtualMemory || !dimGlobals.MmCopyVirtualMemory)
+	if (!dimGlobals.ZwProtectVirtualMemory || !dimGlobals.MmCopyVirtualMemory) {
+		KdPrint((DRIVER_PREFIX "Failed to get critical functions.\n"));
 		return status;
+	}
 
 	// Copying the values to local variables before they are unaccesible because of KeStackAttachProcess.
 	WCHAR* moduleName = (WCHAR*)ExAllocatePool(PagedPool, (wcslen(ModuleToPatch->ModuleName) + 1) * sizeof(WCHAR));
@@ -34,7 +36,7 @@ NTSTATUS PatchModule(PatchedModule* ModuleToPatch) {
 	if (!moduleName)
 		return status;
 	memcpy(moduleName, ModuleToPatch->ModuleName, (wcslen(ModuleToPatch->ModuleName) + 1) * sizeof(WCHAR));
-	
+
 	CHAR* functionName = (CHAR*)ExAllocatePool(PagedPool, strlen(ModuleToPatch->FunctionName) + 1);
 
 	if (!functionName) {
@@ -43,24 +45,19 @@ NTSTATUS PatchModule(PatchedModule* ModuleToPatch) {
 	}
 	memcpy(functionName, ModuleToPatch->FunctionName, strlen(ModuleToPatch->FunctionName) + 1);
 
-	CHAR* patch = (CHAR*)ExAllocatePool(PagedPool, strlen(ModuleToPatch->Patch));
-
-	if (!patch) {
+	if (PsLookupProcessByProcessId((HANDLE)ModuleToPatch->Pid, &TargetProcess) != STATUS_SUCCESS) {
 		ExFreePool(functionName);
 		ExFreePool(moduleName);
 		return status;
 	}
-	memcpy(functionName, ModuleToPatch->FunctionName, strlen(ModuleToPatch->Patch));
 
 	// Getting the PEB.
-	if (PsLookupProcessByProcessId((HANDLE)ModuleToPatch->Pid, &TargetProcess) != STATUS_SUCCESS)
-		return status;
-
 	KeStackAttachProcess(TargetProcess, &state);
 	PREALPEB targetPeb = (PREALPEB)dimGlobals.PsGetProcessPeb(TargetProcess);
 
 	if (!targetPeb) {
 		KdPrint((DRIVER_PREFIX "Failed to get PEB.\n"));
+		KeUnstackDetachProcess(&state);
 		goto CleanUp;
 	}
 
@@ -71,6 +68,7 @@ NTSTATUS PatchModule(PatchedModule* ModuleToPatch) {
 
 	if (!targetPeb->LoaderData) {
 		KdPrint((DRIVER_PREFIX "Failed to get LDR.\n"));
+		KeUnstackDetachProcess(&state);
 		goto CleanUp;
 	}
 
@@ -89,6 +87,7 @@ NTSTATUS PatchModule(PatchedModule* ModuleToPatch) {
 
 	if (!moduleImageBase) {
 		KdPrint((DRIVER_PREFIX "Failed to get image base.\n"));
+		KeUnstackDetachProcess(&state);
 		goto CleanUp;
 	}
 
@@ -96,18 +95,24 @@ NTSTATUS PatchModule(PatchedModule* ModuleToPatch) {
 	PIMAGE_DOS_HEADER dosHeader = (PIMAGE_DOS_HEADER)moduleImageBase;
 
 	// Checking that the image is valid PE file.
-	if (dosHeader->e_magic != IMAGE_DOS_SIGNATURE)
+	if (dosHeader->e_magic != IMAGE_DOS_SIGNATURE) {
+		KeUnstackDetachProcess(&state);
 		goto CleanUp;
+	}
 
 	PFULL_IMAGE_NT_HEADERS ntHeaders = (PFULL_IMAGE_NT_HEADERS)((PUCHAR)moduleImageBase + dosHeader->e_lfanew);
 
-	if (ntHeaders->Signature != IMAGE_NT_SIGNATURE)
+	if (ntHeaders->Signature != IMAGE_NT_SIGNATURE) {
+		KeUnstackDetachProcess(&state);
 		goto CleanUp;
+	}
 
 	IMAGE_OPTIONAL_HEADER optionalHeader = ntHeaders->OptionalHeader;
 
-	if (optionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress == 0)
+	if (optionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress == 0) {
+		KeUnstackDetachProcess(&state);
 		goto CleanUp;
+	}
 
 	// Iterating the export directory.
 	PIMAGE_EXPORT_DIRECTORY exportDirectory = (PIMAGE_EXPORT_DIRECTORY)((PUCHAR)moduleImageBase + optionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress);
@@ -118,56 +123,55 @@ NTSTATUS PatchModule(PatchedModule* ModuleToPatch) {
 
 	for (DWORD j = 0; j < exportDirectory->NumberOfNames; j++) {
 		if (_stricmp((char*)((PUCHAR)moduleImageBase + names[j]), functionName) == 0) {
-			if (ObOpenObjectByPointer(TargetProcess, OBJ_KERNEL_HANDLE, NULL, PROCESS_ALL_ACCESS, *PsProcessType, KernelMode, &hTargetProcess) != STATUS_SUCCESS) {
-				KdPrint((DRIVER_PREFIX "Failed to get process to handle.\n"));
-				success = false;
-				break;
-			}
-
-			// auto patchLen = strlen(patch);
-			SIZE_T patchLen = 6;
-			auto functionAddress = (PVOID)((PUCHAR)moduleImageBase + addresses[ordinals[j]]);
-
-			// Adding write permissions.
-			status = dimGlobals.ZwProtectVirtualMemory(hTargetProcess, &functionAddress, &patchLen, PAGE_EXECUTE_READWRITE, &oldProtection);
-
-			if (status != STATUS_SUCCESS) {
-				KdPrint((DRIVER_PREFIX "Failed to change protection, (0x%08X).\n", status));
-				ZwClose(hTargetProcess);
-				success = false;
-				break;
-			}
-
-			// Patching the function.
-			SIZE_T written;
-
-			// status = dimGlobals.MmCopyVirtualMemory(PsGetCurrentProcess(), pmGlobals.ModulesList.Modules[i].Patch, Process, functionAddress, patchLen, KernelMode, &written);
-			char testPatch[6] = { 0xB8, 0x57, 0x00, 0x07, 0x80, 0xC3 };
-			status = dimGlobals.MmCopyVirtualMemory(PsGetCurrentProcess(), testPatch, TargetProcess, functionAddress, 6, KernelMode, &written);
-
-			if (status != STATUS_SUCCESS) {
-				KdPrint((DRIVER_PREFIX "MmCopyVirtualMemory failed status, (0x%08X).\n", status));
-				success = false;
-			}
-			else
-				KdPrint((DRIVER_PREFIX "Patched function #5.\n"));
-
-			// Restoring permissions and cleaning up.
-			dimGlobals.ZwProtectVirtualMemory(hTargetProcess, &functionAddress, &patchLen, oldProtection, &oldProtection);
-
-			ZwClose(hTargetProcess);
-			KdPrint((DRIVER_PREFIX "Cleaned up #6.\n"));
+			functionAddress = (PUCHAR)moduleImageBase + addresses[ordinals[j]];
 			break;
 		}
 	}
 
-	if (success)
-		status = STATUS_SUCCESS;
-CleanUp:
+	if (!functionAddress) {
+		KdPrint((DRIVER_PREFIX "Failed to get function's address.\n"));
+		KeUnstackDetachProcess(&state);
+		goto CleanUp;
+	}
 	KeUnstackDetachProcess(&state);
+
+	// Adding write permissions.
+	if (ObOpenObjectByPointer(TargetProcess, OBJ_KERNEL_HANDLE, NULL, PROCESS_ALL_ACCESS, *PsProcessType, UserMode, &hTargetProcess) != STATUS_SUCCESS) {
+		KdPrint((DRIVER_PREFIX "Failed to get process to handle.\n"));
+		goto CleanUp;
+	}
+
+	SIZE_T patchLen = (SIZE_T)ModuleToPatch->PatchLength;
+	PVOID functionAddressToProtect = functionAddress;
+	KdPrint((DRIVER_PREFIX "functionAddressToProtect is %p functionAddress is %p.\n", functionAddressToProtect, functionAddress));
+	status = dimGlobals.ZwProtectVirtualMemory(hTargetProcess, &functionAddressToProtect, &patchLen, PAGE_EXECUTE_READWRITE, &oldProtection);
+
+	if (status != STATUS_SUCCESS) {
+		KdPrint((DRIVER_PREFIX "Failed to change protection, (0x%08X).\n", status));
+		ZwClose(hTargetProcess);
+		goto CleanUp;
+	}
+	ZwClose(hTargetProcess);
+	KdPrint((DRIVER_PREFIX "functionAddressToProtect is %p functionAddress is %p.\n", functionAddressToProtect, functionAddress));
+
+	// Patching the function.
+	patchLen = (SIZE_T)ModuleToPatch->PatchLength;
+	
+	status = dimGlobals.MmCopyVirtualMemory(PsGetCurrentProcess(), ModuleToPatch->Patch, TargetProcess, functionAddress, patchLen, KernelMode, &written);
+
+	if (status != STATUS_SUCCESS)
+		KdPrint((DRIVER_PREFIX "MmCopyVirtualMemory failed status, (0x%08X).\n", status));
+
+	// Restoring permissions and cleaning up.
+	if (ObOpenObjectByPointer(TargetProcess, OBJ_KERNEL_HANDLE, NULL, PROCESS_ALL_ACCESS, *PsProcessType, UserMode, &hTargetProcess) == STATUS_SUCCESS) {
+		dimGlobals.ZwProtectVirtualMemory(hTargetProcess, &functionAddressToProtect, &patchLen, oldProtection, &oldProtection);
+		ZwClose(hTargetProcess);
+		KdPrint((DRIVER_PREFIX "Everything is OK.\n"));
+	}
+
+CleanUp:
 	ExFreePool(moduleName);
 	ExFreePool(functionName);
-	ExFreePool(patch);
 	ObDereferenceObject(TargetProcess);
 	return status;
 }
