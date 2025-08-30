25187ae586dada9660d8eae674a9e9f4201c5d19
idov31 <62358580+Idov31@users.noreply.github.com>
2025-08-30 13:39:10 +0300

    Added new type of allocator

--- Nidhogg/MemoryAllocator.hpp
+++ Nidhogg/MemoryAllocator.hpp
@@ -1,6 +1,5 @@
 #pragma once
 #include "pch.h"
-#include "MemoryHelper.hpp"
 
 template<typename DataType>
 class MemoryAllocator {
@@ -57,5 +56,47 @@ public:
 			status = bytesWritten == Size ? STATUS_SUCCESS : STATUS_INVALID_BUFFER_SIZE;
 		}
 		return status;
+	}
+};
+
+template<typename DataType>
+class WindowsMemoryAllocator {
+private:
+	HANDLE processHandle;
+	PVOID baseAddress;
+	SIZE_T allocatedSize;
+	ULONG freeType;
+
+public:
+	_IRQL_requires_max_(APC_LEVEL)
+	WindowsMemoryAllocator(_In_ HANDLE processHandle, _Inout_ PVOID baseAddress, _Inout_ SIZE_T* size, _In_ ULONG allocationType,
+		_In_ ULONG freeType, _In_ ULONG protection, _Out_ NTSTATUS* status) noexcept {
+		this->processHandle = processHandle;
+		this->baseAddress = nullptr;
+		this->allocatedSize = *size;
+		this->freeType = freeType;
+
+		if (size != 0 && processHandle != 0) {
+			*status = ZwAllocateVirtualMemory(processHandle, &this->baseAddress, 0, size,
+				allocationType, protection);
+
+			if (NT_SUCCESS(status)) {
+				this->baseAddress = baseAddress;
+				memset(this->baseAddress, 0, allocatedSize);
+			}
+		}
+	}
+
+	_IRQL_requires_max_(APC_LEVEL)
+	WindowsMemoryAllocator(_In_ PVOID baseAddress, _Inout_ SIZE_T* size, _In_ ULONG protection, _Out_ NTSTATUS* status) noexcept :
+	WindowsMemoryAllocator(ZwCurrentProcess(), baseAddress, size, MEM_COMMIT | MEM_RESERVE, MEM_DECOMMIT, protection, status) {
+
+	}
+
+	_IRQL_requires_max_(APC_LEVEL)
+	~WindowsMemoryAllocator() noexcept {
+		if (this->baseAddress) {
+			ZwFreeVirtualMemory(this->processHandle, &this->baseAddress, &this->allocatedSize, this->freeType);
+		}
 	}
 };
