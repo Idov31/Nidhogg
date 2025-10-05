6e1872e965e48651ce323846d5edee9065e2a911
idov31 <62358580+Idov31@users.noreply.github.com>
2025-10-05 23:29:00 +0300

    Added IrqlGuard

---
+++ Nidhogg/IrqlGuard.h
@@ -0,0 +1,46 @@
+#pragma once
+#include "pch.h"
+
+class IrqlGuard {
+private:
+	KIRQL originalIrql;
+	bool set;
+public:
+	_IRQL_requires_max_(HIGH_LEVEL)
+	IrqlGuard() {
+		set = false;
+		originalIrql = KeGetCurrentIrql();
+	}
+
+	_IRQL_requires_max_(HIGH_LEVEL)
+	IrqlGuard(_In_ KIRQL newIrql) {
+		set = false;
+		originalIrql = KeGetCurrentIrql();
+
+		if (originalIrql != newIrql) {
+			__writecr8(newIrql);
+			set = true;
+		}
+	}
+
+	_IRQL_requires_max_(HIGH_LEVEL)
+	void SetIrql(_In_ KIRQL newIrql) {
+		if (originalIrql != newIrql) {
+			__writecr8(newIrql);
+			set = true;
+		}
+	}
+
+	_IRQL_requires_max_(HIGH_LEVEL)
+	void UnsetIrql() {
+		if (set) {
+			set = false;
+			__writecr8(originalIrql);
+		}
+	}
+
+	_IRQL_requires_max_(HIGH_LEVEL)
+	~IrqlGuard() {
+		UnsetIrql();
+	}
+};
